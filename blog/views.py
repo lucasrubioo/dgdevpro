3d23a0bd6e2c2776eb7c971ec6374ab802ffe783
lucasrubioo <lucassilvarubio99@gmail.com>
2024-03-20 17:36:15 -0300

    inicio de path / views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def portao(request):
+    return HttpResponse("Você chegou ao portão da casa")
+
+def sala(request):
+    return  HttpResponse("Você chegou a sala")
