74f2bf3b4bf7556e98aac21895a5b874cfb1fc3e
lucasrubioo <lucassilvarubio99@gmail.com>
2024-03-21 16:40:42 -0300

    templates

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    # path('', views.portao), - Exemplos
-    # path('sala', views.sala)
+    path('', views.portao), # Exemplos
+    path('sala', views.sala)
 ]
