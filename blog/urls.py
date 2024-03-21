f3ce1e7431a6b9dcf0b6ff461377e5f05def46da
lucasrubioo <lucassilvarubio99@gmail.com>
2024-03-21 16:54:23 -0300

    templates

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('', views.portao), # Exemplos
+    path('portao', views.portao), # Exemplos
     path('sala', views.sala)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
 # Exemplos de como funciona os views. (Tudo aquilo que recebe um request e retorna um response)
 def portao(request):
     return HttpResponse("Você chegou ao portão da casa")
@@ -8,5 +11,5 @@ def portao(request):
 def sala(request):
     return  HttpResponse("Você chegou a sala")
 
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+def porta(request):
+    return render(request, 'blog/porta.html', {})
