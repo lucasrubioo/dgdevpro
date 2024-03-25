927525fa4ecb4fbe251bdd8e7479163f6014f3d1
lucasrubioo <lucassilvarubio99@gmail.com>
2024-03-25 18:18:30 -0300

    Pegando os dados do back-end e subindo a informação para o front-end

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('porta', views.porta), # Exemplos
-    path('sala', views.sala)
+    path('', views.post_list, name='post_list')
+
+    
+    #path('porta', views.porta), - Exemplos de como criar url para ser exibida pelo views
+    #path('sala', views.sala)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+from django.utils import timezone
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {"posts": posts})
+
+
+
+
+
+
 
 # Exemplos de como funciona os views. (Tudo aquilo que recebe um request e retorna um response)
 
-def porta(request):
-    return render(request, 'blog/porta.html', {})
+# def porta(request):
+#    return render(request, 'blog/porta.html', {})
 
-def sala(request):
-    return  HttpResponse("Você chegou a sala")
+# def sala(request):
+#    return  HttpResponse("Você chegou a sala")
